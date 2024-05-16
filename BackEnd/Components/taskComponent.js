ec6939f28bf629cbdf72275fa8e004f601938767
halwohamda <halwohamda@gmail.com>
2024-05-16 11:43:58 -0800

    done

--- BackEnd/Components/taskComponent.js
+++ BackEnd/Components/taskComponent.js
@@ -1,2 +1,23 @@
 import {PrismaClient} from '@prisma/client';
 const prisma = new PrismaClient();
+
+export const newTasks = async(req,res)=>{
+    const {title , description} = req.body;
+    const newTask = await prisma.tasks.create({
+        data:{
+            title,
+            description
+        }
+    })
+    res.status(201).json({
+        message: 'success Added a newTask'
+    })
+
+}
+export const getAllTasks = async(req,res)=>{
+    const tasks = await prisma.tasks.findMany({})
+    res.status(200).json({
+        message: "success gated all tasks"
+    
+    })
+}
