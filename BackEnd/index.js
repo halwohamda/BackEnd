a998af3d7c348c45e1e5a5747cb83a493b7d1fda
halwohamda <halwohamda@gmail.com>
2024-05-16 11:50:49 -0800

    done

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,8 +4,8 @@ const port = 8000;
 import cors from 'cors';
 app.use(cors())
 app.use(express.json())
-
-
+import router from "./router/routers.js";
+app.use('/',tasksRoute)
 
 app.listen(port, ()=>{
     console.log(`listening on ${port}`);
