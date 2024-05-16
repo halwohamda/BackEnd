01679aa55591ef9733c07157c48d3838a7f43411
halwohamda <halwohamda@gmail.com>
2024-05-16 11:49:33 -0800

    done

--- BackEnd/router/routers.js
+++ BackEnd/router/routers.js
@@ -1,2 +1,13 @@
 import express from 'express';
 const router = express.Router();
+import { newTasks,getAllTasks } from '../Components/taskComponent.js';
+
+
+//post
+router.post('/new' ,newTasks)
+
+//get 
+router.get('/',getAllTasks)
+
+
+export default router;
