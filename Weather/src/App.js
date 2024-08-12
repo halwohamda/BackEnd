8ae158c7e72809b6fd44632995da1eeac63a392d
halwohamda <halwohamda@gmail.com>
2024-08-12 12:49:29 -0800

    Done

--- Weather/src/App.js
+++ Weather/src/App.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require ("express");
 const hbs = require ("hbs");
-const request = require("path")
+const path = require("path")
 
 
 const app = express();
-const port = 4000;
-app.get("/",(res,req)=>{
-    res.setEncoding("hello this is my first app")
-})
+
+const port = process.env.PORT ||4000;
+
+app.get("/", (req,res)=>{
+    res.send("hello this is my first app");
+});
 app.listen(port,()=>{
   console.log("listeninig server" +port)
 })
